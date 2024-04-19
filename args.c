f4452747c515be8885c0583f1341aa9dee4a6417
coolpe1788@gmail.com <coolpe1788@gmail.com>
2024-04-19 11:10:54 +0700

    Добавленна программа, работающая с аргументами командной строки

---
+++ args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(int argc, char *argv[], char *envp[]) {
+        printf("Количество параметров программы %d\n", argc);
+        for (int i=0; i<argc; i++) {
+                printf("Параметр %d: %s\n", i, argv[i]);
+        }
+        int j=0;
+        while (envp[j]!=NULL) {
+                printf("Значение параметра %d: %s\n", j, envp[j]);
+                j++;
+        }
+        return 0;
+}
