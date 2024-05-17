32a3cb8a2ef3b58cef4218f8df50b603b957658c
coolpe1788@gmail.com <coolpe1788@gmail.com>
2024-05-17 11:45:22 +0700

    Задукоментированный создане файла fifo добавили fifo.fifo

--- pipe.c
+++ pipe.c
@@ -4,11 +4,10 @@
 #include <stdlib.h>
 
 int main() {
-	int fd[2];
+	int fd[2], result;
 	size_t size;
 	char string[] = "Hello from pipe!\n";
 	char resstring[17];
-	result = fork();
 	if (pipe(fd)<0) {
 		printf("Не удалось создать pipe\n");
 		exit(-1);
@@ -19,36 +18,36 @@ int main() {
                 exit(-1);
         }
 	else if (result > 0) {
-		if(close (fd[0])<0) {
-		 printf("Не удалось закрыть pipe для чтения\n");
-                exit(-1);
-        }
-   else if (result > 0) {
+		if(close(fd[0])<0) {
+			printf("Не удалось закрыть pipe для чтения\n");
+			exit(-1);
+		}
                 if(close (fd[1])<0) {
-                 printf("Не удалось закрыть pipe для записи\n");
-                exit(-1);
-        }
-
-	if(close(fd[1])<0) {
-
-	size = write(fd[1], string, 17);
-	if (size != 17) {
- 		printf("Не удалось записать 17 байт из строки\n");
-		exit(-1);
+                 	printf("Не удалось закрыть pipe для записи\n");
+               		exit(-1);
+        	}
+		size = write(fd[1], string, 17);
+		if (size != 17) {
+ 			printf("Не удалось записать 17 байт из строки\n");
+			exit(-1);
+		}
+		printf("Родитель записал строку pipe\n");
 	}
-	size = read(fd[0], resstring, 17);
-	if (size != 17) {
-		printf("Не удалось прочитать 17 байт из pipe\n");
-		exit(-1);
+	else {
+		if (close(fd[1])<0) {
+        	        printf("Не удалось закрыть pipe для записи\n");
+                	exit(-1);
+        	}
+	        size = read(fd[0], resstring, 17);
+	        if (size != 17) {
+	                printf("Не удалось прочитать 17 байт из pipe\n");
+        	        exit(-1);
+        	}
+        	printf("Прочитанная строка %s\n", resstring);
+        	if (close(fd[0])<0) {
+                	printf("Не удалось закрыть pipe для чтения\n");
+                	exit(-1);
+        	}
 	}
-	printf("Прочитанная строка %s\n", resstring);
-	if (close(fd[0])<0) {
-		printf("Не удалось закрыть pipe для чтения\n");
-		exit(-1);
-	}
-	if (close(fd[1])<0) {
-                printf("Не удалось закрыть pipe для записи\n");
-                exit(-1);
-        }
 	return 0;
 }
